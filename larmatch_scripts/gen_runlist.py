52e3cef32cdf3b0ad3f9747166c14fd0c5cd78f7
twongjirad <taritree.wongjirad@gmail.com>
2021-01-22 16:04:05 -0500

    add check and runlist script. cpu now uses 4 cores to speed things up drastatically

---
+++ larmatch_scripts/gen_runlist.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+import os,sys,re
+
+
+#samplename = "mcc9jan_run1_bnb5e19"
+#inputlist="../run1inputlists/mcc9jan_run1_bnb5e19.list"
+
+samplename="mcc9_v29e_dl_run3_G1_extbnb_dlana"
+inputlist="../maskrcnn_input_filelists/mcc9_v29e_dl_run3_G1_extbnb_dlana_MRCNN_INPUTS_LIST.txt"
+
+
+outfolder="../../data/v0/%s/larmatch/"%(samplename)
+
+# get list of finished files
+cmd = "find %s -name larmatch_kps_*.root -size +1000k | sort" % (outfolder)
+print(cmd)
+plist = os.popen(cmd)
+flist = plist.readlines()
+
+finished = []
+
+for f in flist:
+    f = f.strip()
+    #print(f)
+    base = os.path.basename(f)
+    #print(base.split("fileid")[-1])
+    x = re.split("[_-]+",base.split("fileid")[-1])
+    #print(x)
+    jobid = int(x[0])
+    finished.append(jobid)
+    #print(jobid," ",base)
+
+finished.sort()
+print("Number of finished files: ",len(finished))
+
+pnjobs = os.popen("cat %s | wc -l"%(inputlist))
+njobs = int(pnjobs.readlines()[0])
+
+print("Number of files in input list: ",njobs)
+missing = []
+for i in range(njobs):
+    if i not in finished:
+        missing.append(i)
+missing.sort()        
+print("Number of missing files: ",len(missing))
+
+foutname="larmatch_runlist_%s.txt"%(samplename)
+print("Making file, %s, with file IDs to run"%(foutname))
+fout = open(foutname,'w')
+for fileid in missing:
+    print("%d"%(fileid),file=fout)
+fout.close()
+
+
--- larmatch_scripts/run_batch_kps_larmatch_cpu.sh
+++ larmatch_scripts/run_batch_kps_larmatch_cpu.sh
@@ -5,11 +5,11 @@ STRIDE=$2
 SAMPLE_NAME=$3
 INPUTLIST=$4
 INPUTSTEM=$5
+FILEIDLIST=$6
 
 # we assume we are already in the container
 
 WORKDIR=/cluster/tufts/wongjiradlab/nutufts/dlgen2prod/larmatch_scripts/
-#UBDL_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl/
 UBDL_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl_py3
 LARMATCH_DIR=${UBDL_DIR}/larflow/larmatchnet/
 WEIGHTS_DIR=/cluster/tufts/wongjiradlab/twongj01/ubdl/larflow/larmatchnet/grid_deploy_scripts/larmatch_kps_weights/
@@ -27,22 +27,23 @@ cudadev="cpu"
 echo "JOB ARRAYID: ${SLURM_ARRAY_TASK_ID} : DEVICE = ${cudadev}"
 
 # LOCAL JOBDIR
-local_jobdir=`printf /tmp/larmatch_kps_jobid%04d_${SAMPLE_NAME} ${SLURM_ARRAY_TASK_ID}`
+local_jobdir=`printf /tmp/larmatch_kps_jobid%d_%04d_${SAMPLE_NAME} ${SlURM_JOB_ID} ${SLURM_ARRAY_TASK_ID}`
 #echo "local jobdir: $local_jobdir"
 rm -rf $local_jobdir
 mkdir -p $local_jobdir
 
 # local log file
-local_logfile=`printf larmatch_kps_${SAMPLE_NAME}_jobid%04d.log ${SLURM_ARRAY_TASK_ID}`
+local_logfile=`printf larmatch_kps_${SAMPLE_NAME}_jobid%d_%04d.log ${SLURM_JOB_ID} ${SLURM_ARRAY_TASK_ID}`
 #echo "output logfile: "$local_logfile
 
 #echo "SETUP CONTAINER/ENVIRONMENT"
 cd ${UBDL_DIR}
 alias python=python3
 source /usr/local/root/root-6.22.06/bin/thisroot.sh
-source setenv_py3.sh
-source configure.sh
+source setenv_py3.sh > /dev/null
+source configure.sh > /dev/null
 export PYTHONPATH=${LARMATCH_DIR}:${PYTHONPATH}
+export OMP_NUM_THREADS=4
 
 cd $local_jobdir
 
@@ -52,30 +53,36 @@ echo "STARTING TASK ARRAY ${SLURM_ARRAY_TASK_ID} for ${SAMPLE_NAME}" > ${local_l
 # run a loop
 for ((i=0;i<${STRIDE};i++)); do
 
+    # CALC JOB ID
     jobid=$(( ${start_jobid} + ${i} ))
     echo "JOBID ${jobid}" >> ${local_logfile}
+
+    # GET FILE ID
+    let lineno=${jobid}+1    
+    let fileid=`sed -n ${lineno}p ${FILEIDLIST}`
   
     # GET INPUT FILENAME
-    let lineno=${jobid}+1
-    inputfile=`sed -n ${lineno}p ${INPUTLIST}`
+    let filelineno=${fileid}+1
+    inputfile=`sed -n ${filelineno}p ${INPUTLIST}`
     baseinput=$(basename $inputfile )
     echo "inputfile path: $inputfile" >> ${local_logfile}
     echo "baseinput: $baseinput" >> ${local_logfile}
 
+    echo "JOBID ${jobid} running FILEID ${fileid} with file: ${baseinput}"
+
     # local outfile
     jobname=`printf jobid%04d ${jobid}`
-    fileid=`printf fileid%04d ${jobid}`
     local_outfile=$(echo $baseinput  | sed 's|'"${INPUTSTEM}"'|larmatch_kps_'"${fileid}"'|g')
     local_basename=$(echo $baseinput | sed 's|'"${INPUTSTEM}"'|larmatch_kps_'"${fileid}"'|g' | sed 's|.root||g')
     echo "outfile : "$local_outfile >> ${local_logfile}
     scp $inputfile $baseinput
     
-    CMD="python $LARMATCH_DIR/deploy_kps_larmatch.py --supera $baseinput --weights ${WEIGHTS_DIR}/${WEIGHT_FILE} --output $local_outfile --min-score 0.5 --adc-name wire --chstatus-name wire --device-name $cudadev --use-unet -tb"
+    CMD="python $LARMATCH_DIR/deploy_kps_larmatch.py --supera $baseinput --weights ${WEIGHTS_DIR}/${WEIGHT_FILE} --output $local_outfile --min-score 0.5 --adc-name wire --chstatus-name wire --device-name $cudadev --use-unet --use-skip-limit 1000000 -tb"
     echo $CMD >> ${local_logfile}
     $CMD >> ${local_logfile} 2>&1
 
     # subfolder dir
-    let nsubdir=${jobid}/100
+    let nsubdir=${fileid}/100
     subdir=`printf %03d ${nsubdir}`
 
     # copy to subdir in order to keep number of files per folder less than 100. better for file system.
